0d83c721fb4ba8d9a1c1a14fc3536160e17ea4c5
danielnuwin <35547522+danielnuwin@users.noreply.github.com>
2023-12-21 16:05:41 -0800

    Final Test Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 //RAFCE shortcut to create react component
 const App = () => {
   return (
-    <h1>Welcome to ProShop Testing Git Commit 2</h1>
+    <h1>Welcome to Ecommerce App</h1>
   )
 }
 
