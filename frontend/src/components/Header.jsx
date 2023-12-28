7a055f3e6ac709a7c6127e9e42264c030765c44c
danielnuwin <35547522+danielnuwin@users.noreply.github.com>
2023-12-27 20:55:04 -0800

    Added Product Lists and Cards to homescreen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
-    return(
+    return (
         <header>
             <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Daniel's Store</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt='Proshop' />
+                        Daniel's Store</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className='ms-auto'>
-                          <Nav.Link href='/cart'> <FaShoppingCart/> Cart </Nav.Link>
-                          <Nav.Link href='/login'> <FaUser/> Sign In </Nav.Link>
+                            <Nav.Link href='/cart'> <FaShoppingCart /> Cart </Nav.Link>
+                            <Nav.Link href='/login'> <FaUser /> Sign In </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </header>        
+        </header>
     );
 }
 
